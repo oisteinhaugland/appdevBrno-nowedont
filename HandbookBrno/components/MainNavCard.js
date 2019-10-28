2efe91787d71c06b5619916821f99366327cf49d
DomantasMk <45693077+DomantasMk@users.noreply.github.com>
2019-10-28 13:49:02 +0200

    Added TouchableHighlights on MainNavCards

--- HandbookBrno/components/MainNavCard.js
+++ HandbookBrno/components/MainNavCard.js
@@ -1,37 +1,35 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
 
 const MainNavCard = ({Title, Description, Color}) => {
+    /*Just return the style for the NavCard with a specified Color in props */
     const NavColor = (color) =>{
         if(color == null){
             return styles.NavCardStyle
         }
         else{
             return {
-                borderRadius: 10,
-                backgroundColor: color,
-                padding:12,
-                marginLeft:10,
-                marginRight:10,
-                marginTop:10,
+                ...styles.NavCardStyle, backgroundColor: color,
               };
         }
     }
     return (
-        <View style={NavColor(Color)}>
-            <Text style={styles.Title}>{Title}</Text>
-            <Text>{Description}</Text>
-        </View>
+        <TouchableHighlight underlayColor="white" activeOpacity={0.85} onPress={() =>{ /*This is where the function to go to other view will be */}}>
+            <View style={NavColor(Color)}>
+                 <Text style={styles.Title}>{Title}</Text>
+                <Text>{Description}</Text>
+            </View>
+        </TouchableHighlight>
     )
 }
 const styles = StyleSheet.create({
     NavCardStyle: {
       borderRadius: 10,
-      backgroundColor: 'red',
       padding:12,
       marginLeft:10,
       marginRight:10,
       marginTop:10,
+      backgroundColor:"red",
     },
     Title:{
         fontSize:23,
