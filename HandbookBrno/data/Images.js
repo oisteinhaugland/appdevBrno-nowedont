dcbe07ddad9e8d365b03683797194723f72c84bc
Gabriela <xchmel27@stud.fit.vutbr.cz>
2019-12-09 17:21:51 +0100

    some work on cathegory landmarks and universities

--- HandbookBrno/data/Images.js
+++ HandbookBrno/data/Images.js
@@ -2,6 +2,10 @@ const images = [
     {
       imgName: 'ForeignPoliceMap', 
       uri: require('../data/Images/BrnoForeignPoliceMap.png')
+    },
+    {
+      imgName: 'LibertySquare', 
+      uri: require('../data/Images/LibertySquare.png')
     }
 ]
   
